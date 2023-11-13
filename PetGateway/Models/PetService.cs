52f473d1692cfe7da0c158e5318383097a857246
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-11-12 21:37:03 -0600

    1. Added ConfigurePetServices 2. Added Owner views 3. Added Owner controller

---
+++ PetGateway/Models/PetService.cs
@@ -0,0 +1,9 @@
+﻿namespace PetGateway.Models
+{
+    public class PetService
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; } = string.Empty;
+        public int Cost { get; set; }
+    }
+}
