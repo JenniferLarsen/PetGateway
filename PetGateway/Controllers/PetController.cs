a8f1f5a6f5dfbbae31e33e6958b288584343bc80
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-11-13 20:18:50 -0600

    troubleshooting edit pet

--- PetGateway/Controllers/PetController.cs
+++ PetGateway/Controllers/PetController.cs
@@ -10,6 +10,7 @@ namespace PetGateway.Controllers
         public PetController(GatewayContext ctx) => context = ctx;
 
         //view all pets for a specific owner
+        [HttpGet]
         public IActionResult Index(int ownerId)
         {
             var owner = context.Owners
@@ -31,29 +32,12 @@ namespace PetGateway.Controllers
         [HttpGet]
         public IActionResult Add(int ownerId)
         {
-            ViewBag.OwnerId = ownerId;
-            return View();
-        }
-
-        // Process the form submission to add a new pet
-        [HttpPost]
-        public IActionResult Add(int ownerId, Pet pet)
-        {
-            if (ModelState.IsValid)
-            {
-                pet.OwnerId = ownerId;
-                context.Pets.Add(pet);
-                context.SaveChangesAsync();
-                return RedirectToAction("Index", new { ownerId });
-            }
-
-            ViewBag.OwnerId = ownerId;
-            return View(pet);
-        }
+            ViewBag.Action = "Add";
+            return View("Edit", new Pet());
+        }  
 
         // Display the form to edit a specific pet
-        [HttpGet]
-        [Route("pet/Edit/{id}")]
+        [HttpGet]       
         public IActionResult Edit(int id)
         {
             var pet = context.Pets.Find(id);
@@ -63,55 +47,34 @@ namespace PetGateway.Controllers
                 return NotFound();
             }
 
-            // Get the owner information
-            var owner = context.Owners.Find(pet.OwnerId);
-            // Set owner information in ViewBag
-            ViewBag.OwnerFullName = $"{owner.FirstName} {owner.LastName}";
+
 
             return View(pet);
         }
 
-        [HttpPost]
-        [Route("pet/Edit/{id}")]
-        public IActionResult Edit(int id, Pet pet)
+        [HttpPost]       
+        public IActionResult Edit(Pet pet)
         {
             if (ModelState.IsValid)
             {
-                if (id == 0)
+                if (pet.PetId== 0)
                 {
                     // Adding a new pet
-                    pet.OwnerId = ViewBag.OwnerId;
+                    pet.OwnerId = ViewBag.OwnerId;//may not need
                     context.Pets.Add(pet);
                 }
                 else
-                {
-                    // Editing an existing pet
-                    var existingPet = context.Pets.Find(id);
+                {                             
 
-                    if (existingPet == null)
-                    {
-                        return NotFound();
-                    }
-
-                    // Update existing pet properties
-                    existingPet.PetName = pet.PetName;
-                    existingPet.PetType = pet.PetType;
-                    existingPet.PetColor = pet.PetColor;
-                    existingPet.PetBreed = pet.PetBreed;
-                    existingPet.PetAge = pet.PetAge;
-                    existingPet.PetGender = pet.PetGender;
-                    existingPet.PetWeight = pet.PetWeight;
-                    existingPet.Spayed_Neutered = pet.Spayed_Neutered;
-
-                    context.Pets.Update(existingPet);
+                    context.Pets.Update(pet);
                 }
 
                 context.SaveChanges();
-                return RedirectToAction("Index", new { ownerId = ViewBag.OwnerId });
+                return RedirectToAction("Index",new { ownerId = pet.OwnerId });
             }
 
-            ViewBag.Action = (id == 0) ? "Add" : "Edit";
-            ViewBag.OwnerId = pet.OwnerId;
+            ViewBag.Action = (pet.PetId == 0) ? "Add" : "Edit";
+            //ViewBag.OwnerId = pet.OwnerId;
             return View(pet);
         }
 
